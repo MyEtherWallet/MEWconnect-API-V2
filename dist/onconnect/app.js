92429d57748548a22dad761b2aa70a17d12467aa
Gage Siuniak <gsiuniak@gmail.com>
2019-01-09 18:18:21 -0800

    First commit

---
+++ dist/onconnect/app.js
@@ -0,0 +1,34 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.handler = undefined;
+
+var _awsSdk = require('aws-sdk');
+
+var _awsSdk2 = _interopRequireDefault(_awsSdk);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+_awsSdk2.default.config.update({ region: process.env.AWS_REGION });
+var DDB = new _awsSdk2.default.DynamoDB({ apiVersion: '2012-10-08' });
+
+var handler = function handler(event, context, callback) {
+  var putParams = {
+    TableName: process.env.TABLE_NAME,
+    Item: {
+      connectionId: {
+        S: event.requestContext.connectionId
+      }
+    }
+  };
+  DDB.putItem(putParams, function (err) {
+    callback(null, {
+      statusCode: err ? 500 : 200,
+      body: err ? 'Failed to connect: ' + JSON.stringify(err) : 'Connected'
+    });
+  });
+};
+
+exports.handler = handler;
---
+++ src/onconnect/app.js
@@ -0,0 +1,24 @@
+'use strict'
+
+import AWS from 'aws-sdk'
+AWS.config.update({ region: process.env.AWS_REGION })
+const DDB = new AWS.DynamoDB({ apiVersion: '2012-10-08' })
+
+const handler = (event, context, callback) => {
+  const putParams = {
+    TableName: process.env.TABLE_NAME,
+    Item: {
+      connectionId: { 
+        S: event.requestContext.connectionId 
+      }
+    }
+  }
+  DDB.putItem(putParams, err => {
+    callback(null, {
+      statusCode: err ? 500 : 200,
+      body: err ? `Failed to connect: ${JSON.stringify(err)}` : 'Connected'
+    })
+  })
+}
+
+export { handler }
